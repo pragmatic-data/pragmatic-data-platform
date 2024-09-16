fee02ef7c6545d13343544ebcac5574b5786cc77
rz70rz70@gmail.com <rz70rz70@gmail.com>
2024-09-16 08:55:07 +0000

    Improved name for variable in test

--- integration_tests/models/structural/storage/stage/TEST_STAGE__EXTERNAL_CONFIG_EXTENDED.sql
+++ integration_tests/models/structural/storage/stage/TEST_STAGE__EXTERNAL_CONFIG_EXTENDED.sql
@@ -1,7 +1,7 @@
 {{ pragmatic_data.stage(
     source_model            = ref('GENERIC_TWO_COLUMN_TABLE'),
     source                  = config.require('source'),
-    calculated_columns      = config.require('x_calculated_columns'),
+    calculated_columns      = config.require('flattened_extended_calculated_columns'),
     hashed_columns          = config.require('hashed_columns'),
     default_records         = config.require('default_records'),
     remove_duplicates       = config.require('remove_duplicates')
