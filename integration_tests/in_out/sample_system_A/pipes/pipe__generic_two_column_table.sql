0d37d06251c30e53d95d56db00142a8c9913b06e
Roberto Zagni <rz70rz70@gmail.com>
2026-02-17 15:58:24 +0000

    Added ability to configure and create pipes and related landing tables

--- integration_tests/in_out/sample_system_A/pipes/pipe__generic_two_column_table.sql
+++ integration_tests/in_out/sample_system_A/pipes/pipe__generic_two_column_table.sql
@@ -1,31 +1,42 @@
-{% macro pipe__generic_two_column_table(recreate_table = false) %}
+{% macro pipe__generic_two_column_table(recreate_table = true) %}
 
 {%- set yaml_str -%}
 landing_table:
     db_name:     "{{ get_SYSTEM_A_inout_db_name() }}"
     schema_name: "{{ get_SYSTEM_A_inout_schema_name() }}"
-    table_name:  GENERIC_TWO_COLUMN_TABLE
+    table_name:  PIPED_TWO_COLUMN_TABLE
     columns: #-- Define the landing table columns and eventually its type (& constraints - same syntax as in Create Table).
         - COLUMN1                       # -- NO data type => TEXT
         - COLUMN2: NUMBER NOT NULL      # -- defines column2 of type Number with NOT NULL constraint
 
-pipe:
-    integration: NOTIFICATION_INTEGRATION_NAME              # -- Name of the notification integration to be used by the pipe
-    stage_name: "{{ get_SYSTEM_A_inout_stage_name() }}"     # -- "PDP_TEST.LAND_SYSTEM_A.SYSTEM_A__STAGE"
-    # format_name:                                          # -- No format to use the format defined in the stage - put format to override
-    # pattern: '.*.csv.gz'      #-- optional
+pipe:                                                       # -- We always create pipes in the same schema as the LT they load data into
+    stage_name: "{{ get_SYSTEM_A_inout_stage_name() }}"     # -- Fully qualified name "PDP_TEST.LAND_SYSTEM_A.SYSTEM_A__STAGE", always needed.
+    # integration: NOTIFICATION_INTEGRATION_NAME            # -- Name of the notification integration to be used by the pipe, needed for AUTO ingestion!
+
+    # pipe_name: my_pipe            # -- Optional, base name (not FQ) of pipe; default is the Landing Table name, with _PIPE appended
+    # format_name:                  # -- Optional, if empty use the format defined in the stage - provide value to override
+    # stage_path:                   # -- Optional, subpath in the stage - to filter the files loaded by the pipe (string path, quick)
+    # pattern: '.*.csv.gz'          # -- Optional, to filter the files loaded by the pipe (regexp, slow)
+    # add_file_content_key: True    # -- Optional, add the content key metadata; true by default, false if explicitly set to falsey (false, null, empty).
+
+    # -- Field definition NEEDED FOR SEMI STRUCTURED inputs - Map the variant internal columns to desired explicit columns
+    # field_expressions:         # -- NO expressions needed for CSV; the number of fields for $1, ...$n is taken from the list of columns.
+    #   - COLUMN1: $1::TEXT
+    #   - COLUMN2: $2::NUMBER
+    #   - src_data: $1          # -- the full record as a Variant column (note - the LT would need an extra VARIANT column)
 
 # -- Sample full FILE PATH of file to be ingested
 # -- '@"PDP_TEST"."LAND_SYSTEM_A"."SYSTEM_A__STAGE"/SYSTEM_A/generic/2024_12_15/GENERIC_TWO_COLUMN_TABLE___0_0_0.csv.gz'
+# -- Run 'ALTER PIPE PDP_TEST.LAND_SYSTEM_A.PIPED_TWO_COLUMN_TABLE__PIPE REFRESH;' to trigger the ingestion of the files in the LT.
 
 {%- endset -%}
 
-{%- set metadata_dict = fromyaml(yaml_str) -%}
+{%- set cfg = fromyaml(yaml_str) -%}
 
-{% do pragmatic_data.run_CSV_ingestion(
-        landing_table_dict = metadata_dict['landing_table'],
-        ingestion_dict  = metadata_dict['ingestion'],
-        recreate_table = recreate_table
+{% do pragmatic_data.run_create_pipe(
+        landing_table_dict  = cfg['landing_table'],
+        pipe_dict           = cfg['pipe'],
+        recreate_table      = recreate_table
 ) %}
 
 {% endmacro %}
--- macros/in_out/ingestion_lib/helpers/copy_into.sql
+++ macros/in_out/ingestion_lib/helpers/copy_into.sql
@@ -1,13 +1,13 @@
 {% macro copy_into__sql(
-        full_table_name, 
-        field_definitions, 
-        full_stage_name, 
-        stage_path = '/', 
-        file_pattern = none, 
-        full_format_name = none,
-        ingestion_ts_utc = run_started_at,
-        add_file_content_key = false
-    ) %}
+    full_table_name, 
+    field_definitions, 
+    full_stage_name, 
+    stage_path = '/', 
+    file_pattern = none, 
+    full_format_name = none,
+    ingestion_ts_utc = run_started_at,
+    add_file_content_key = false
+) %}
 
 COPY INTO {{ full_table_name }}
 FROM (
@@ -22,8 +22,8 @@ FROM (
         {%- else %}
         CONVERT_TIMEZONE('UTC', METADATA$START_SCAN_TIME)::TIMESTAMP_NTZ    as INGESTION_TS_UTC
         {%- endif %}
-        {%- if add_file_content_key %}
-        , METADATA$FILE_CONTENT_KEY                                           as FILE_CONTENT_KEY
+        {%- if add_file_content_key %}, 
+        METADATA$FILE_CONTENT_KEY                                           as FILE_CONTENT_KEY
         {%- endif %}
 
     FROM @{{ full_stage_name }}{{ stage_path }}
@@ -31,7 +31,6 @@ FROM (
 {%- if file_pattern %}
 PATTERN = '{{ file_pattern }}'
 {%- endif %}
-{%- if file_pattern and full_format_name %}, {% endif %}
 {%- if full_format_name %}
 FILE_FORMAT = (FORMAT_NAME = '{{ full_format_name }}')
 {%- endif %}
